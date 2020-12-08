3bf7f287c6e99c986fd503cbc9c8368779ea42e4
Lionel <la92@njit.edu>
2020-12-08 13:17:05 -0500

    Changes on application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.assets.initialize_on_precompile = false
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
